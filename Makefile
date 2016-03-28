073afd35a62e32d5f70eff9a38d2ab99dc78a08b
nihlaeth <info@nihlaeth.nl>
2016-03-29 01:25:55 +0200

    Test rtrd

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 rtrd: rtrd.c cronparser.c sockets.c daemonize.c pickfile.c
 	gcc -o bin/rtrd rtrd.c cronparser.c sockets.c daemonize.c pickfile.c
 
+test-rtrd: testrtrd.c sockets.c
+	gcc -o bin/testrtrd testrtrd.c sockets.c
+
 test: testdaemon.c testpickfile.c testcronparser.c daemonize.c cronparser.c pickfile.c testclient.c testserver.c sockets.c
 	gcc -o bin/testpickfile testpickfile.c cronparser.c pickfile.c 
 	gcc -o bin/testcronparser testcronparser.c cronparser.c
---
+++ testrtrd.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2016 Nihlaeth
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include "sockets.h"
+
+void send_msg(int socketfd, char* msg) {
+    if (send(socketfd, msg, strlen(msg) + 1, 0) == -1) {
+        printf("Failed to send message |%s|\n", msg);
+    } else {
+        printf("Sent message |%s|\n", msg);
+    }
+}
+
+char* receive_msg(int socketfd) {
+    int status;
+    char buf[128];
+    if ((status = recv(socketfd, &buf, 128, 0)) > 0) {
+        buf[status] = '\0';
+        printf("Received message |%s|\n", buf);
+        char *return_str = malloc(strlen(buf) + 1);
+        strcpy(return_str, buf);
+        return return_str;
+    } else {
+        if (status < 0) {
+            printf("Could not receive message.\n");
+        } else {
+            printf("Server closed connection.\n");
+            exit(EXIT_SUCCESS);
+        }
+    }
+}
+
+void client_conn(int socketfd) {
+    send_msg(socketfd, "CURRENT\n");
+    receive_msg(socketfd);
+    send_msg(socketfd, "REQFILE\n");
+    receive_msg(socketfd);
+    send_msg(socketfd, "CURRENT\n");
+    receive_msg(socketfd);
+    send_msg(socketfd, "NEXT\n");
+    receive_msg(socketfd);
+    send_msg(socketfd, "YES?\n");
+    receive_msg(socketfd);
+    send_msg(socketfd, "PAUSE\n");
+    receive_msg(socketfd);
+    send_msg(socketfd, "STOP\n");
+    receive_msg(socketfd);
+    send_msg(socketfd, "YES?\n");
+    receive_msg(socketfd);
+}
+
+void main() {
+    printf("Starting client\n");
+    client_socket("/tmp/rtrd", *client_conn);
+    printf("Stopped client\n");
+}
