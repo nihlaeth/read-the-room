7c65e9dbb181d24665d0ecc875019ce6b308d2f5
nihlaeth <info@nihlaeth.nl>
2016-03-29 12:14:19 +0200

    runrtrdtest: add delays between starting server and test

--- runrtrdtest.sh
+++ runrtrdtest.sh
@@ -4,8 +4,10 @@ make rtrd
 make test-rtrd
 echo "\n\nstart rtrd:\n"
 ./bin/rtrd
+sleep 1
 echo "\n\nstart testrtrd:\n"
 ./bin/testrtrd
+sleep 1
 echo "\n\n/var/log/messages:\n"
 tail -n 10 /var/log/messages
 echo "\n\n/var/log/syslog:\n"
